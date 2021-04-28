f8018b77c73afe116c18a37aabebff472052e25f
Rafeeq CE <cerafeeq@gmail.com>
2021-04-28 20:19:25 +0530

    feat: removed express.json() middleware

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,6 +1,6 @@
-const Patient = require('../models/patient');
 const JSONStream = require('JSONStream');
 const es = require('event-stream');
+const Patient = require('../models/patient');
 
 let getStream = function(request) {
 	let parser = JSONStream.parse('*');
@@ -10,7 +10,6 @@ let getStream = function(request) {
 exports.addPatientInfo = (req, res) => {
 	getStream(req)
 		.pipe(es.mapSync(function (data) {
-			console.log('adding patients');
 			Patient.addPatientInfo(data.Gender, data.HeightCm, data.WeightKg, (err) => {
 				if (err) {
 					res.status(400).json({
